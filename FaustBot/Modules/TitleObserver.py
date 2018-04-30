70b7ff2b79c171b0e31178abd4f3c72823e332e0
niansa <anton-sa@web.de>
2018-04-30 12:00:11 +0200

    Update TitleObserver.py

--- FaustBot/Modules/TitleObserver.py
+++ FaustBot/Modules/TitleObserver.py
@@ -1,7 +1,8 @@
 import html
+import requests
 import re
 import urllib
-from urllib import request
+from bs4 import BeautifulSoup
 
 from FaustBot.Communication.Connection import Connection
 from FaustBot.Modules.PrivMsgObserverPrototype import PrivMsgObserverPrototype
@@ -26,7 +27,7 @@ class TitleObserver(PrivMsgObserverPrototype):
                 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)'}
                 url = url
                 req = urllib.request.Request(url, None, headers)
-                resource = urllib.request.urlopen(req)
+                resource = BeautifulSoup(urllib.request.urlopen(req), "html.parser")
                 title = self.getTitle(resource)
                 print(title)
                 title = title[:250]
@@ -36,18 +37,7 @@ class TitleObserver(PrivMsgObserverPrototype):
                 pass
             
     def getTitle(self, resource):
-            encoding = resource.headers.get_content_charset()
-            # der erste Fall kann raus, wenn ein anderer Channel benutzt wird
-            if resource.geturl().find('rehakids.de') != -1:
-                encoding = 'windows-1252'
-            if not encoding:
-                encoding = 'utf-8'
-            content = resource.read().decode(encoding, errors='replace')
-            if resource.geturl().find('twitter.com') != -1:
-                title_re = re.compile('<meta  property="og:description" content="(.+?)">')
-            else:
-                title_re = re.compile("<title>(.+?)</title>")
-            title = title_re.search(content).group(1)
+            title = resource.title.string
             title = html.unescape(title)
             title = title.replace('\n', ' ').replace('\r', '')
             title = title.replace("&lt;", "<")
